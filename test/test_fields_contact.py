76661d98cfdad0ef6981de2804ecdf312718c5d9
Anastasia <summer.90@bk.ru>
2024-01-05 16:09:37 +0300

    Исправила: теперь app.contact.get_contact_info_by_id(contact.id) вызывается 1 раз в течение итерации цикла

--- test/test_fields_contact.py
+++ test/test_fields_contact.py
@@ -4,12 +4,12 @@ from random import randrange
 def test_fields_contact(app,orm):
     old_contacts = orm.get_contact_list()
     for contact in old_contacts:
-        assert contact.last_name == app.contact.get_contact_info_by_id(contact.id).last_name
-        assert contact.first_name == app.contact.get_contact_info_by_id(contact.id).first_name
-        assert contact.address == app.contact.get_contact_info_by_id(contact.id).address
-        assert merge_phones_like_on_home_page(contact) == app.contact.get_contact_info_by_id(contact.id).all_phones_from_home_page
-        assert merge_emails_on_home_page(contact) == app.contact.get_contact_info_by_id(
-            contact.id).all_emails
+        contact_from_home_page = app.contact.get_contact_info_by_id(contact.id)
+        assert contact.last_name == contact_from_home_page.last_name
+        assert contact.first_name == contact_from_home_page.first_name
+        assert contact.address == contact_from_home_page.address
+        assert merge_phones_like_on_home_page(contact) == contact_from_home_page.all_phones_from_home_page
+        assert merge_emails_on_home_page(contact) == contact_from_home_page.all_emails
 
    # contact_from_home_page = app.contact.get_contact_list()[index]
    # contact_from_edit_page = app.contact.get_contact_info_from_edit_page_by_id(id)
