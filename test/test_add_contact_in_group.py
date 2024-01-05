c58ff173f07056e0727140bf508652145e374e2a
Anastasia <summer.90@bk.ru>
2024-01-05 17:54:31 +0300

    Добавлены предусловия для тестов добавления и удаления контакта в/из группы

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -1,14 +1,46 @@
 from model.group import Group
+from model.contact import Contact
 import random
-
 
 def test_add_contact_in_group(app,orm,check_ui):
     old_contacts = orm.get_contact_list()
     old_groups = orm.get_group_list()
-    contact = random.choice(old_contacts)
-    group = random.choice(old_groups)
-    app.contact.add_contact_in_group_by_id(contact.id,group.id)
-    contacts_in_group = orm.get_contacts_in_group((Group(id=group.id)))
-    assert contact in contacts_in_group
+    if len(old_groups) == 0:
+        app.group.create(Group(name="test"))
+        old_groups = orm.get_group_list()
+    if len(old_contacts) == 0:
+        app.contact.add_contact(Contact("Ivanov","Ivan","Ivanov","ivan"))
+        old_contacts = orm.get_contact_list()
+    n=0
+    for group in old_groups:
+        contacts_out_of_group = orm.get_contacts_not_in_group(group)
+        if len(contacts_out_of_group) != 0:
+            contact = random.choice(contacts_out_of_group)
+            app.contact.add_contact_in_group_by_id(contact.id, group.id)
+            contacts_in_group = orm.get_contacts_in_group((Group(id=group.id)))
+            assert contact in contacts_in_group
+            break
+        else:
+            n+=1
+            continue
+    if n == len(old_groups):
+        print("Все контакты добавлены в группы!")
+        app.group.create(Group(name="test"))
+        new_group = orm.get_group_list()[-1]
+        contact = random.choice(old_contacts)
+        app.contact.add_contact_in_group_by_id(contact.id, new_group.id)
+        contacts_in_new_group = orm.get_contacts_in_group(new_group)
+        assert contact in contacts_in_new_group
+
+
+
+
+
+    # contacts_out_of_group = orm.get_contacts_not_in_group()
+    # if len(contacts_out_of_group)!= 0:
+    #     contact = random.choice(contacts_out_of_group)
+    #     app.contact.add_contact_in_group_by_id(contact.id, group.id)
+    #     contacts_in_group = orm.get_contacts_in_group((Group(id=group.id)))
+    #     assert contact in contacts_in_group
 
 
--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -1,11 +1,19 @@
 import random
+from model.group import Group
+from model.contact import Contact
 
 
 def test_delete_contact_from_group(app,orm,check_ui):
     old_groups = orm.get_group_list()
+    old_contacts = orm.get_contact_list()
+    if len(old_groups) == 0:
+        app.group.create(Group(name="test"))
+        old_groups = orm.get_group_list()
+    if len(old_contacts) == 0:
+        app.contact.add_contact(Contact("Ivanov","Ivan","Ivanov","ivan"))
+        old_contacts = orm.get_contact_list()
     group = random.choice(old_groups)
     if len(orm.get_contacts_in_group(group)) == 0:
-        old_contacts = orm.get_contact_list()
         contact_for_add_group = random.choice(old_contacts)
         app.contact.add_contact_in_group_by_id(contact_for_add_group.id, group.id)
     contact = random.choice(orm.get_contacts_in_group(group))
