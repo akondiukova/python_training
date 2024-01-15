bd37739540157d78b4ab3a66c086903e87eaed78
Anastasia <summer.90@bk.ru>
2024-01-15 22:43:03 +0300

    в файл requirements.txt добавлен pytest-bdd

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -1,6 +1,6 @@
 from pytest_bdd import scenario
-from group_steps import *
+from .group_steps import *
 
-@scenario('bdd/groups.feature', 'Add new group')
+@scenario('groups.feature','Add new group')
 def test_add_new_group():
     pass
