871a894648a4c02c92cdeacd967fc3df2599fa33
Anastasia <summer.90@bk.ru>
2024-01-15 13:15:27 +0300

    добавлен файл requirements.txt и сценарий теста bdd

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -23,30 +23,6 @@ def test_add_contact_in_group(app,orm,check_ui):
     assert rand_contact in contacts_in_group
 
 
-    '''
-    n=0
-    for group in old_groups:
-        contacts_out_of_group = orm.get_contacts_not_in_group(group)
-        if len(contacts_out_of_group) != 0:
-            contact = random.choice(contacts_out_of_group)
-            app.contact.add_contact_in_group_by_id(contact.id, group.id)
-            contacts_in_group = orm.get_contacts_in_group((Group(id=group.id)))
-            assert contact in contacts_in_group
-            break
-        else:
-            n+=1
-            continue
-    if n == len(old_groups):
-        print("Все контакты добавлены в группы!")
-        app.group.create(Group(name="test"))
-        new_group = orm.get_group_list()[-1]
-        contact = random.choice(old_contacts)
-        app.contact.add_contact_in_group_by_id(contact.id, new_group.id)
-        contacts_in_new_group = orm.get_contacts_in_group(new_group)
-        assert contact in contacts_in_new_group
-    '''
-
-
 
 
 
